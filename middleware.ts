5e7811b7f141ca7baf611146fb15bdb561e57ebc
Fenyvesi Botond <botond.fenyvesi@gmail.com>
2025-11-20 14:21:58 +0100

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -23,6 +23,7 @@ export async function middleware(req: NextRequest) {
             name,
             value: "",
             ...options,
+            maxAge: 0,
           });
         },
       },
